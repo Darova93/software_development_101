e3603edd39b321ee1d2a2bde74f8d449867d9fb5
alnavarroh <alan.navarro@bajatechnologies.com>
2024-01-09 11:40:02 -0800

    Solution for problem 27 in JS

---
+++ algorithm_exercises/27. Remove Element/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    let length = nums.length
+    let i = 0
+    while (i < length) {
+        if (nums[i] === val) {
+            nums.splice(i, 1)
+        }
+        else {
+            i++
+        }
+    }
+    return nums.length
+};
