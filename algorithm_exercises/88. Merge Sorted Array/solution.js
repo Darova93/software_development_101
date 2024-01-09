49d7328d1568b90351f2c9d4b51f659b052429d7
alnavarroh <alan.navarro@bajatechnologies.com>
2024-01-09 11:35:30 -0800

    addressing pr comments

--- algorithm_exercises/88. Merge Sorted Array/solution.js
+++ algorithm_exercises/88. Merge Sorted Array/solution.js
@@ -6,17 +6,17 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
+    let i = m--
+    let j = n--
     let k = m + n - 1
-    m--
-    n--
-    while (n >= 0) {
-        if (nums1[m] > nums2[n]) {
-            nums1[k] = nums1[m]
-            m--
+    while (j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i]
+            i--
         }
         else {
-            nums1[k] = nums2[n]
-            n--
+            nums1[k] = nums2[j]
+            j--
         }
         k--
     }
