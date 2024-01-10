ee6cb618bd790a59a4efbbd3239fc2d9ae4b331c
Eduardo Solorio <Solorioe.93@gmail.com>
2024-01-09 17:11:39 -0800

    Correcciones hechas

--- algorithm_exercises/876. Middle of the Linked List/solution.py
+++ algorithm_exercises/876. Middle of the Linked List/solution.py
@@ -1,12 +1,6 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curr = curr2 = head
-        while curr2 and curr2.next:
-            curr = curr.next
-            curr2 = curr2.next.next
-        return curr
+def middleNode(head):
+    curr = curr2 = head
+    while curr2 and curr2.next:
+        curr = curr.next
+        curr2 = curr2.next.next
+    return curr
