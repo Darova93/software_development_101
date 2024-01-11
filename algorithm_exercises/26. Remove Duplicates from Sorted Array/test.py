8d763a861ea22719db8eff733030e8814da9e3ea
Eduardo Solorio <Solorioe.93@gmail.com>
2024-01-11 10:31:23 -0800

    Se agrego test.py

---
+++ algorithm_exercises/26. Remove Duplicates from Sorted Array/test.py
@@ -0,0 +1,8 @@
+from solution import removeDuplicates
+
+def test_removeDuplicates():
+    assert removeDuplicates([1,1,2]) == 2
+
+def test_removeDuplicates2():
+    assert removeDuplicates([0,0,1,1,1,2,2,3,3,4]) == 5
+
