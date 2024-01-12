4652e4bc0d558640a5ec8de39b5a0e927114b1a9
Eduardo Solorio <Solorioe.93@gmail.com>
2024-01-11 17:35:42 -0800

    comentarios borrados

--- algorithm_exercises/26. Remove Duplicates from Sorted Array/test_26.py
+++ algorithm_exercises/26. Remove Duplicates from Sorted Array/test_26.py
@@ -5,12 +5,4 @@ import pytest
 def test_removeDuplicates(nums, expectedNums):
     result = removeDuplicates(nums)
     assert result == expectedNums
-
-
-"""
-def test_removeDuplicates():
-    assert removeDuplicates([1,1,2]) == 2
-
-def test2_removeDuplicates():
-    assert removeDuplicates([0,0,1,1,1,2,2,3,3,4]) == 5
-"""
+    
