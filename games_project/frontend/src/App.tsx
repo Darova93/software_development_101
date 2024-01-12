89add3706c8c1b462f082d03368666b04d739234
David Rodriguez Vazquez <darova93@gmail.com>
2024-01-11 19:02:27 -0800

    Added starter code for flask and React

---
+++ games_project/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Example from './components/example';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Example />
+      </header>
+    </div>
+  );
+}
+
+export default App;
