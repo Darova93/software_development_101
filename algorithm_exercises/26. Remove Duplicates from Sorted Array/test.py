dc76c8fb39a02f32b1b56db4c5d0146bd8081e7a
Eduardo Solorio <Solorioe.93@gmail.com>
2024-01-11 17:30:05 -0800

    parametrize

--- algorithm_exercises/26. Remove Duplicates from Sorted Array/test.py
+++
@@ -1,7 +0,0 @@
-from solution import removeDuplicates
-
-def test_removeDuplicates():
-    assert removeDuplicates([1,1,2]) == 2
-
-def test2_removeDuplicates():
-    assert removeDuplicates([0,0,1,1,1,2,2,3,3,4]) == 5
