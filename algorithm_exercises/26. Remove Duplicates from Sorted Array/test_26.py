d4f8a8d200ca9027d33481abe72b5676b62a80ef
Eduardo <84428517+Eduardo-93@users.noreply.github.com>
2024-01-16 16:08:44 -0800

    Exercise/26/python (#11)
    * Problema 26 resuelto

---
+++ algorithm_exercises/26. Remove Duplicates from Sorted Array/test_26.py
@@ -0,0 +1,7 @@
+from solution import removeDuplicates
+import pytest
+
+@pytest.mark.parametrize("nums, expectedNums", [([1,1,2], 2), ([0,0,1,1,1,2,2,3,3,4], 5)])
+def test_removeDuplicates(nums, expectedNums):
+    result = removeDuplicates(nums)
+    assert result == expectedNums
