13c3b36bc5450c8e76f999d40c6fdc6fe0c18beb
Eduardo Solorio <Solorioe.93@gmail.com>
2024-01-16 18:53:52 -0800

    word of the day

--- games_project/backend/wordle.py
+++ games_project/backend/wordle.py
@@ -1,15 +1,15 @@
-def wordle(word: str):
+def wordle(word: str, wordOfTheDay: str):
     word.lower()
     missplaced = []
     fails = []
     correct = []
-    if word == "lunes":
+    if word == wordOfTheDay:
         missplaced = [] 
         fails = []
         correct = [0, 1, 2, 3, 4]
         return missplaced, fails, correct
     for idx in word:
-        
 
 
-print(wordle("Tacos"))
+
+print(wordle("Tacos", "lunes"))
