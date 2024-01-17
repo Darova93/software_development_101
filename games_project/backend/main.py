4e41f6e7a7dd4bf520292837e56f59f8fb2f4871
danielalbinoms <danielalbinoms@gmail.com>
2024-01-16 18:31:44 -0800

    backend/venv/activation

--- games_project/backend/main.py
+++ games_project/backend/main.py
@@ -1,15 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/", methods=["GET"])
+@app.route("/v0.1/api/wordle/checkword", methods=["POST"])
 @cross_origin()
 def hello_world():
+    print(request.get_json())
     response = {
+        
         "message": "Hello",
         "code": 200,
-        "author": "kubix"
+        "author": "kubixxx"
     }
     return jsonify(response)
