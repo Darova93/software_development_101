d60d695f4d86c873dfca524e46051dd691b964be
danielalbinoms <danielalbinoms@gmail.com>
2024-01-16 19:47:11 -0800

    wordle1

--- games_project/backend/wordle.py
+++ games_project/backend/wordle.py
@@ -1,16 +1,24 @@
 def wordle(word: str, wordOfTheDay: str):
     word.lower()
     missplaced = []
-    
-    fails = []
     correct = []
+    wordDict = {"M":1, "A":2, "R":1, "I":1}
+
+    fails = []
     if word == wordOfTheDay:
         missplaced = [] 
         fails = []
         correct = [0, 1, 2, 3, 4]
         return missplaced, fails, correct
-    for idx in word:
+    else:
+    for index in range(0, len(wordOfTheDay)):
+        if word[index] == wordOfTheDay[index]:
+            correct.append(index)
+        elif word[index] in wordOfTheDay:
+            missplaced.append(index) #AGUAS CON VARIAS LETRAS IGUALES
+        else:
+            fails.append(index)
+        
+    return correct
 
-
-
-print(wordle("Tacos", "lunes"))
+print(wordle("MARIA", "caras"))
