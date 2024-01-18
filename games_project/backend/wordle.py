0c7dce62230ec66967a46e31503baac2646ddffb
danielalbinoms <danielalbinoms@gmail.com>
2024-01-18 10:05:14 -0800

    XDD

--- games_project/backend/wordle.py
+++ games_project/backend/wordle.py
@@ -6,13 +6,14 @@ def wordle(testWord: str, wordOfTheDay: str):
     wrong = []
     #letterCount = {letter : wordOfTheDay.count(letter) for letter in set(wordOfTheDay)}
     testLetters = list(testWord)
+    remainingLetters = wordOfTheDay
     for index in range(0, len(wordOfTheDay)):
         if testWord[index] == wordOfTheDay[index]:
             right.append(index)
             testLetters[index] = 0
-            wordOfTheDay.replace(testWord[index],'',1)
+            remainingLetters.replace(testWord[index],'',1)
     for index in range(0,len(wordOfTheDay)):
-        if testLetters[index] in wordOfTheDay:
+        if testLetters[index] in remainingLetters:
             missplaced.append(index)
             wordOfTheDay.replace(testWord[index],'',1)
         elif testLetters[index] != 0:
