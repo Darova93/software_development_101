32440085905d54f20a00e401339eca643d4b7e4e
Eduardo Solorio <solorioe.93@gmail.com>
2024-01-18 01:12:36 -0800

    trabajando logica wordle

--- games_project/backend/public/import_text.py
+++ games_project/backend/public/import_text.py
@@ -1,5 +1,4 @@
 import random
-import time
 
 with open('palabras_rae.txt', 'r', newline="\r\n", encoding="utf-8") as words:
     def read(listWords):
@@ -7,11 +6,37 @@ with open('palabras_rae.txt', 'r', newline="\r\n", encoding="utf-8") as words:
         for word in listWords:
             words_rae.append(word.replace("\r\n", ""))
         return words_rae
-    
-    wordOfDay = random.choice(read(words))
+    dictionary = read(words)
+    wordOfDay = (random.choice(dictionary)).upper()
 
 #Palabra random cada que se ejecuta el programa    
 print(wordOfDay)
+correct = []
+missplaced = []
+fails = []
 
-#Hora local, formas de fijarlo a fecha
-print(time.localtime())
+guess = str(input())
+print(guess.upper())
+if guess not in dictionary:
+    print("Se necesita agregar una palabra de 5 letras")
+elif len(guess) > 5 or len(guess) < 5:
+    print("Se necesita una palabra de 5 letras")
+elif guess == wordOfDay:
+    correct = [0,1,2,3,4]
+    missplaced = []
+    fails = []
+else:
+    print(f'La palabra del dia es: {wordOfDay}')
+    print(f'La palabra guess es: {guess.upper()}')
+    round = 6
+    for index in range(5):
+        if guess[index] == wordOfDay[index]:
+            correct.append(index)
+        elif guess[index] != wordOfDay[index]  and guess[index] in wordOfDay:
+            missplaced.append(index)
+        else:
+            fails.append(index)
+
+print(f'Posicion de las letras correctas: {correct}')
+print(f'Posicion de las letras incorrectas: {missplaced}')
+print(f'Posicion de las letras no encontradas: {fails}')
