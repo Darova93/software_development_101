ace3cd1f4b427e9fd7b13667c130026aa1077114
danielalbinoms <danielalbinoms@gmail.com>
2024-01-18 10:24:13 -0800

    backend/wordle/dictionary

--- games_project/backend/wordle.py
+++ games_project/backend/wordle.py
@@ -1,21 +1,30 @@
-def wordle(testWord: str, wordOfTheDay: str):
-    testWord.upper()
+def wordle(testingWord: str, wordOfTheDay: str):
+    testingWord.upper()
     wordOfTheDay.upper()
     right = []
     missplaced = []
     wrong = []
-    #letterCount = {letter : wordOfTheDay.count(letter) for letter in set(wordOfTheDay)}
-    testLetters = list(testWord)
+    letterCount = {letter : wordOfTheDay.count(letter) for letter in set(wordOfTheDay)}
+    testLetters = list(testingWord)
     remainingLetters = wordOfTheDay
     for index in range(0, len(wordOfTheDay)):
-        if testWord[index] == wordOfTheDay[index]:
+        if testingWord[index] == wordOfTheDay[index]:
             right.append(index)
             testLetters[index] = 0
-            remainingLetters.replace(testWord[index],'',1)
+            remainingLetters.replace(testingWord[index],'',1)
+            letterCount[testingWord[index]] -= 1
+            
     for index in range(0,len(wordOfTheDay)):
         if testLetters[index] in remainingLetters:
             missplaced.append(index)
-            remainingLetters.replace(testWord[index],'',1)
+            remainingLetters.replace(testingWord[index],'',1)
         elif testLetters[index] != 0:
             wrong.append(index)
+
+        if letterCount[testingWord[index]] > 0 and testingWord[index] in wordOfTheDay:
+            missplaced.append(index)
+        else:
+            wrong.append(index)
+
+
     return right, missplaced, wrong
