f39ecd5d44fe7c0bed9d10fabceb15d0d38da8da
Eduardo Solorio <Solorioe.93@gmail.com>
2024-01-18 20:08:11 -0800

    me rindo por hoy - text random word

--- games_project/backend/public/import_text.py
+++ games_project/backend/public/import_text.py
@@ -1,24 +1,37 @@
 import random
 import string
 
-with open('palabras_rae.txt', 'r', newline="\r\n", encoding="utf-8") as words:
+with open('./palabras_rae.txt', 'r', newline="\r\n", encoding="utf-8") as words:
     def read(listWords):
         words_rae = []
         for word in listWords:
             words_rae.append(word.replace("\r\n", ""))
         return words_rae
-    dictionary = read(words)
-    acentos = ["á", "é", "í", "ó", "ú"]
-    randomWord = random.choice(dictionary)
-    index = 0
-    while True:
-        if index == len(randomWord):
-            wordOfDay = randomWord
-            break
-        elif acentos[index] not in randomWord:
-            index += 1
+    
+    def randomWordWithoutAccent(dictionary):
+        acentos = ["á", "é", "í", "ó", "ú"]
+        randomWord = random.choice(dictionary)
+        index = 0
+        while True:
+            if index == len(randomWord):
+                wordOfDay = randomWord
+                break
+            elif acentos[index] not in randomWord:
+                index += 1
+            else:
+                randomWord = random.choice(dictionary)
+                index = 0
+        return dictionary, wordOfDay
+    dictionary, wordOfDay = randomWordWithoutAccent(read(words))
+"""
+def wordLetterCount(word):
+    letterCount = {}
+    for index in range(len(wordOfDay)):
+        if wordOfDay[index] in letterCount:
+            letterCount[wordOfDay[index]] += 1
         else:
-            randomWord = random.choice(dictionary)
+            letterCount[wordOfDay[index]] = 1
+    return letterCount
 
 def playerGuess(word):
     if len(word) > 5 or len(word) <= 4:
@@ -30,6 +43,15 @@ def playerGuess(word):
                 print("La palabra solo debe tener letras: ")
                 playerGuess(str(input()))
     return word
+
+def getCorrectPosition(guess, wordOfDay):
+    reduceLetter = wordLetterCount(wordOfDay)
+    correct = []
+    for index in range(len(wordOfDay)):
+        if guess[index] == wordOfDay[index]:
+            correct.append(index)
+            reduceLetter[wordOfDay[index]] -= 1
+    return correct, reduceLetter
 
 def wordle(guess, wordOfDay):
     rounds = 1
@@ -45,13 +67,15 @@ def wordle(guess, wordOfDay):
             missplaced = []
             fails = []
             return correct, missplaced, fails
+        correct, letterCount = getCorrectPosition(guess, wordOfDay)
         for index in range(len(wordOfDay)):
-            if guess[index] == wordOfDay[index]:
-                correct.append(index)
-            elif guess[index] != wordOfDay[index] and guess[index] in wordOfDay:
-                missplaced.append(index)
-            else:
+            if guess[index] not in wordOfDay:
                 fails.append(index)
+            elif letterCount[wordOfDay[index]] == 0:
+                fails.append(index)
+            elif (guess[index] != wordOfDay[index] and letterCount[wordOfDay[index]] >= 1) and guess[index] in wordOfDay:
+                letterCount[wordOfDay[index]] -= 1
+                missplaced.append(index)
         print(f'Ronda {rounds} terminada.\nPosicion letras correctas:{correct}.\nPosicion letras mal ubicadas: {missplaced}.\nPosicion letras incorrectas: {fails}.')
         rounds += 1
         if rounds > 6:
@@ -68,3 +92,4 @@ correct, missplaced, fails = wordle(guess, wordOfDay)
 print(f'Posicion de las letras correctas: {correct}')
 print(f'Posicion de las letras incorrectas: {missplaced}')
 print(f'Posicion de las letras no encontradas: {fails}')
+"""
---
+++ games_project/backend/public/test_random_word.py
@@ -0,0 +1,4 @@
+from import_text import randomWordWithoutAccent, read
+import pytest
+
+@pytest.mark.parametrize("randomWord, expectedWord")
