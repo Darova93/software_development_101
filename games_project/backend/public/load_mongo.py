3db15f35028e58364c2afd9a40d1bccd896d7d8e
David Rodriguez Vazquez <david.rodriguez@workera.ai>
2024-01-18 19:12:09 -0800

    Chang Id to _id

--- games_project/backend/public/load_mongo.py
+++ games_project/backend/public/load_mongo.py
@@ -12,7 +12,7 @@ try:
     id = 1
     with open("palabras_rae_completo.txt", "r", encoding="utf-8", newline="\n") as words_file:
         for word in words_file:
-            word_collection.insert_one({"Id": id, "Name": word.replace("\n", "")})
+            word_collection.insert_one({"_id": id, "Name": word.replace("\n", "")})
             id += 1
     print(f"Exported {id} entries")
 except Exception as e:
