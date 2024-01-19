4525bee6b3193718f43190a0ca0e74d34e32407d
danielalbinoms <danielalbinoms@gmail.com>
2024-01-18 17:50:58 -0800

    wordle/class/2

--- games_project/backend/wordle.py
+++ games_project/backend/wordle.py
@@ -1,31 +1,28 @@
 class wordle:
 
-    right = []
-    missplaced = []
-    wrong = []
-    letterCount = {letter : wordOfTheDay.count(letter) for letter in set(wordOfTheDay)}
+    correctLetterIndices = []
+    missplacedLetterIndices = []
+    wrongLetterIndices = []
+    
 
     def __init__(self, guess: str, wordOfTheDay: str):
         self.guess = guess.upper()
         self.wordOfTheDay = wordOfTheDay.upper()
-    
-    def _getRightPositions(guess, wordOfTheDay):
-        for index in range(0, len(wordOfTheDay)):
-            if guess[index] == wordOfTheDay[index]:
-                right.append(index)
-                letterCount[guess[index]] -= 1
-        return right
-    
-    def _getMissplacedAndWrongPositions(guess, wordOfTheDay):
-        for index in range(0,len(wordOfTheDay)):
-            if guess[index] != wordOfTheDay[index]:
-                if  guess[index] in wordOfTheDay and letterCount[guess[index]] > 0:
-                    missplaced.append(index)
-                    letterCount[guess[index]] -= 1
-                else:
-                    wrong.append(index)
-        return missplaced, wrong
-    
-    return (_getRightPositions(guess,wordOfTheDay),_getMissplacedAndWrongPositions(guess,wordOfTheDay))
-    print(wordle('atara','maria'))
+        self.wordOfTheDayLetterCount = {letter : self.wordOfTheDay.count(letter) for letter in set(self.wordOfTheDay)}
 
+    def _getRightPositions(self):
+        for index in range(0, len(self.wordOfTheDay)):
+            if self.guess[index] == self.wordOfTheDay[index]:
+                self.correctLetterIndices.append(index)
+                self.wordOfTheDayLetterCount[self.guess[index]] -= 1
+        return self.correctLetterIndices
+    
+    def _getMissplacedAndWrongPositions(self):
+        for index in range(0,len(self.wordOfTheDay)):
+            if self.guess[index] != self.wordOfTheDay[index]:
+                if  self.guess[index] in self.wordOfTheDay and self.wordOfTheDayLetterCount[self.guess[index]] > 0:
+                    self.missplacedLetterIndices.append(index)
+                    self.wordOfTheDayLetterCount[self.guess[index]] -= 1
+                else:
+                    self.wrongLetterIndices.append(index)
+        return self.missplacedLetterIndices, self.wrongLetterIndices
