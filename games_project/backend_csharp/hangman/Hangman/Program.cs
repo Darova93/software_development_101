4b6c532f3397964cabb379964ffa1ca098d792b1
Brianda <yazzmin74@gmail.com>
2024-01-25 21:27:40 -0800

    Clean program

--- games_project/backend_csharp/hangman/Hangman/Program.cs
+++ games_project/backend_csharp/hangman/Hangman/Program.cs
@@ -3,21 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Hangman;
 using Hangman.Services;
 
-//var connectionString = "mongodb+srv://games-dev:exK0juZnNZJzaJQK@west-cluster.p2tfglm.mongodb.net/?retryWrites=true&w=majority";
-//if (connectionString == null)
-//{
-//    Environment.Exit(0);
-//}
-
-//var client = new MongoClient(connectionString);
-//var dbContextOptions =
-//    new DbContextOptionsBuilder<GamesDbContext>().UseMongoDB(client, "games");
-//var db = new GamesDbContext(dbContextOptions.Options);
-
 var builder = WebApplication.CreateBuilder(args);
-
-
-//builder.Services.AddTransient<IWordService, WordService>();
 
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
