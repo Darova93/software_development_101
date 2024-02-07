99d101ef788e4a8c2642c9fa851b8b756b3c5022
David Rodriguez Vazquez <69552193+Darova93@users.noreply.github.com>
2024-02-06 16:16:31 -0800

    Changes to python backend (#31)
    * Changes to python backend
    - Add SQLAlchemy
    - Add Pyright
    - Add Pytest
    - Add Marshmallow for schema validations
    - Migrates routes to Blueprints
    - Add shell command to start docker
    - Add .env to gitignore
    - Refactored wordle
    - Changed naming convention

--- games_project/backend/app.py
+++ games_project/backend/app.py
@@ -1,26 +1,17 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
-from wordle import todaysWordleGame
-from validation import validateRequest, checkDictionaryListWords, checkSpecialCaractersInValidWords
+from flask import Flask
+from database.connection import db_session, init_db
+from routes import ROUTES
 
+init_db()
 app = Flask(__name__)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
 
-@app.route("/v0.1/api/wordle/checkword", methods=["POST"])
-@cross_origin()
-def checkWord():
-    wordleRequest = request.get_json()
-    isValid = validateRequest(wordleRequest)
-    
-    if not isValid:
-        return wordleRequest
-    checkingRealWords = checkDictionaryListWords(isValid)
-    if not checkingRealWords:
-        return wordleRequest
-    
-    playerAttemts = todaysWordleGame(checkingRealWords)
-    
-    return jsonify(playerAttemts)
+for route in ROUTES:
+    app.register_blueprint(route)
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8000)
