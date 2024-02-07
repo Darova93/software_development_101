3554c53d0514959d74337f06f6094b8cf5a6832c
Alan Navarro <40080155+alnavarroh@users.noreply.github.com>
2024-02-06 16:14:56 -0800

    added localhost (#30)

--- games_project/frontend/src/utils/constants.tsx
+++ games_project/frontend/src/utils/constants.tsx
@@ -1,3 +1,4 @@
-export const PATH_WORDLE_API_KUBO = "http://192.168.100.35:5000/v0.1/api/wordle/checkword"
+export const PATH_WORDLE_API_KUBO = "http://192.168.100.7:5000/v0.1/api/wordle/checkword"
 export const PATH_WORDLE_API_DANI = "http://192.168.100.12:5000/v0.1/api/wordle/checkword"
 export const IP_ALAN = "http://192.168.100.69"
+export const LOCALHOST = "/api/games/v0.1/api/wordle/checkword"
