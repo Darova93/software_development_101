2b2a36fca3cf03562db7c1eca16eea17e7fabc2a
David Rodriguez Vazquez <david.rodriguez@workera.ai>
2024-02-08 20:46:25 -0800

    Removed games_project

--- games_project/frontend/src/games/wordle/wordleLettter.tsx
+++
@@ -1,9 +0,0 @@
-const WordleLetter = ({letter, index, className} : {letter:string, index:number, className:string}) => {
-    return(
-            <div className={`letter ${className}`}>
-                {letter}
-            </div>
-    )
-}
-
-export default WordleLetter
